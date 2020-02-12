535e0b7ad30d8d8f3381b6ac2c9f1bb9818403e8
skanehira <sho19921005@gmail.com>
2020-02-12 22:34:10 +0900

    fix test

--- file_test.go
+++ file_test.go
@@ -17,49 +17,47 @@ func mkfile(name string) error {
 }
 
 func TestFiles(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		testdir, err := ioutil.TempDir("", "")
-		if err != nil {
-			t.Fatalf("cannot create testdir: %s", err)
-		}
-		defer os.RemoveAll(testdir)
+	testdir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("cannot create testdir: %s", err)
+	}
+	defer os.RemoveAll(testdir)
 
-		exceptedFiles := map[string]string{
-			"a.go": "f",
-			"tmp":  "d",
-		}
+	exceptedFiles := map[string]string{
+		"a.go": "f",
+		"tmp":  "d",
+	}
 
-		for f, typ := range exceptedFiles {
-			tmp := filepath.Join(testdir, f)
-			// if file
-			if typ == "f" {
-				err := mkfile(tmp)
-				if err != nil {
-					t.Fatalf("create error: %s", err)
-				}
-				// if dir
-			} else if typ == "d" {
-				err := os.Mkdir(tmp, 0666)
-				if err != nil {
-					t.Fatalf("create error: %s", err)
-				}
+	for f, typ := range exceptedFiles {
+		tmp := filepath.Join(testdir, f)
+		// if file
+		if typ == "f" {
+			err := mkfile(tmp)
+			if err != nil {
+				t.Fatalf("create error: %s", err)
+			}
+			// if dir
+		} else if typ == "d" {
+			err := os.Mkdir(tmp, 0666)
+			if err != nil {
+				t.Fatalf("create error: %s", err)
 			}
 		}
+	}
 
-		files, err := Files(testdir)
-		if err != nil {
-			t.Fatalf("cannot get files: %s", err)
-		}
+	files, err := Files(testdir)
+	if err != nil {
+		t.Fatalf("cannot get files: %s", err)
+	}
 
-		fileName := files[0].Name()
-		if fileName != "a.go" {
-			t.Fatalf("want: a.go, got: %s", fileName)
-		}
-	})
+	fileName := files[0].Name()
+	if fileName != "a.go" {
+		t.Fatalf("want: a.go, got: %s", fileName)
+	}
+}
 
-	t.Run("failed", func(t *testing.T) {
-		if _, err := Files("xxx"); err == nil {
-			t.Fatalf("failed test: err is nil")
-		}
-	})
+func TestFilesFail(t *testing.T) {
+	if _, err := Files("xxx"); err == nil {
+		t.Fatalf("failed test: err is nil")
+	}
 }
